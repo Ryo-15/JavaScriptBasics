56d131236515812d60320a750a225bf57b7f7e52
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-15 14:21:57 +0900

    [finish]lesson7

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
-console.log(10 + 3);
-console.log(10 - 3);
-console.log(10 * 3);
-console.log(10 / 3);
-console.log(10 % 3);
-console.log(10 ** 3);
+console.log(150 * 140);
+console.log(150 * 160);
 
-console.log(2 + 10 * 3);
-console.log((2 + 10) * 3);
+const price = 150;
+
+console.log(price * 140);
+console.log(price * 160);
