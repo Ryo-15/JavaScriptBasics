842b411f2458cc57676cc8942c0ce29ee6139853
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-15 19:46:37 +0900

    [finish]lesson16

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 'use strict';
 
-const score = 60;
-const name = 'taguchi';
+const signal = 'green';
 
-if (score >= 50) {
-  if (name === 'taguchi') {
-    console.log('Good job!');
-  }
-}
-
-if (score >= 50 && name === 'taguchi') {
-  console.log('Good job!');
+switch (signal) {
+  case 'red':
+    console.log('Stop!');
+    break;
+  case 'yellow':
+    console.log('Caution!');
+    break;
+  case 'blue':
+  case 'green':
+    console.log('Go!');
+    break;
+  default:
+    console.log('Wrong signal!');
+    break;
 }
 
