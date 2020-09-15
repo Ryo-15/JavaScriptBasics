dcd3adcb42bdcc4595eed9522f8808b18c209ba9
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-15 17:44:28 +0900

    [finish]lesson12

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 'use strict';
 
-console.log("5" * 2);
-console.log("5" - 2);
-console.log(parseInt("5", 10) + 2);
+const price = 1200;
+
+console.log(price > 1000);
+console.log(price < 1000);
+console.log(price >= 1000);
+console.log(price <= 1000);
+console.log(price === 1000);
+console.log(price !== 1000);
+
+console.log(Boolean(0));
+console.log(Boolean('hello'));
+
 
 
