be508a810a848b1b5db9f1dd3f4c8bfea6c322b1
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-17 15:27:29 +0900

    [finish]basic_javascript_objects_lesson6

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 'use strict';
 
 {
-  const scores = [80, 90, 40, 70];
-  scores.push(60, 50);
-  scores.shift();
-  // 90, 40, 70, 60, 50
+  const otherScores = [10, 20];
+  const scores = [80, 90, 40, 70, ...otherScores];
+  console.log(scores);
 
-  for (let i = 0; i < scores.length; i++) {
-    console.log(`Scores ${i}: ${scores[i]}`);
+  function sum(a, b) {
+    console.log(a + b);
   }
+
+  sum(...otherScores);
 }
