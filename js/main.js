b42a1b881c3974e3ef4c75d357c2178ebfd6aa61
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-20 15:57:49 +0900

    [finish]basic_javascript_objects_lesson12

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 'use strict';
 
 {
-  // const point = [100, 180];
-
-  // const point = {x: 100, y: 180};
   const point = {
     x: 100,
     y: 180,
   };
+
+  point.x = 120;
+  // point['y'] = 190;
+
+  // console.log(point.x);
+  // console.log(point['y']);
+
+  point.z = 90;
+  delete point.y;
+
   console.log(point);
 }
