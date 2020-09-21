b89c007a2d88fc9e8ac6023f93bc8b1b7a3d7913
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-21 17:30:22 +0900

    [finish]basic_javascript_objects_lesson16

--- js/main.js
+++ js/main.js
@@ -6,8 +6,15 @@
   // x = 5;
   // console.log(x);
   // console.log(y);
+
+  // let x = [1, 2];
+  // let y = x;
+  // x[0] = 5;
+  // console.log(x);
+  // console.log(y);
+
   let x = [1, 2];
-  let y = x;
+  let y = [...x];
   x[0] = 5;
   console.log(x);
   console.log(y);
