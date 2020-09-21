7fe7997edf4059ccd5cc11da572203f48ec153aa
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-21 21:18:51 +0900

    [finish]basic_javascript_objects_lesson20

--- js/main.js
+++ js/main.js
@@ -1,22 +1,8 @@
 'use strict';
 
 {
-  const scores = [10, 3, 9];
-  let sum = 0;
-
-  scores.forEach(score => {
-    sum += score;
-  });
-
-  const avg = sum / scores.length;
-
-  console.log(sum);
-  console.log(avg);
-
-  console.log(Math.floor(avg));
-  console.log(Math.ceil(avg));
-  console.log(Math.round(avg));
-  console.log(avg.toFixed(3));
-
   console.log(Math.random());
+
+  // Math.floor(Math.random() * (max + 1 - min)) + min;
+  console.log(Math.floor(Math.random() * (6)) + 1);
 }
