0c5e39611ca3f73c1c743c3e1130e3c862356bec
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-22 17:53:00 +0900

    [finish]basic_javascript_objects_lesson21

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
 {
-  console.log(Math.random());
+  const d = new Date();
 
-  // Math.floor(Math.random() * (max + 1 - min)) + min;
-  console.log(Math.floor(Math.random() * (6)) + 1);
+  console.log(d);
+
+  console.log(`${d.getMonth() + 1} 月 ${d.getDate()} 日`);
 }
