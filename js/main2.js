d6e1e5c0af125369387659ff0ac0d962003bfad4
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-25 13:24:00 +0900

    [finish]basic_javascript_dom_lesson13

--- js/main2.js
+++ js/main2.js
@@ -2,10 +2,13 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    const item1 = document.querySelectorAll('li')[1];
+    const li = document.createElement('li');
+    const text = document.querySelector('input');
 
-    // item1.remove();
-    // 親Node.removeChild(削除するNode)
-    document.querySelector('ul').removeChild(item1);
+    li.textContent = text.value;
+    document.querySelector('ul').appendChild(li);
+
+    text.value = '';
+    text.focus();
   });
 }
